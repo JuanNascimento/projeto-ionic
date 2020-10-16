71df9ba88892eba3c0579e21cae04305787eb176
JuanNascimento <juanzinhopriscila@gmail.com>
2020-10-16 17:01:32 -0300

    Primeira versão

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDlK4ezgqA732GAyu6MFVSUjntm38Fbnbs",
+    authDomain: "projeto-integrador-e9a79.firebaseapp.com",
+    databaseURL: "https://projeto-integrador-e9a79.firebaseio.com",
+    projectId: "projeto-integrador-e9a79",
+    storageBucket: "projeto-integrador-e9a79.appspot.com",
+    messagingSenderId: "488484948649",
+    appId: "1:488484948649:web:978f59bfc267827e50f7b8"
+  }
+  };
+  
